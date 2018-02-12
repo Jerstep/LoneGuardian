6e114dca52ff1abdfd9c2e54f90582e04a969726
Unknown <jeremy.koskamp@live.nl>
2018-02-12 16:39:04 +0100

    typ

---
+++ Assets/Assets/Scripts/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            float speed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = 20;
+            }
+            else
+            {
+                speed = 10;
+            }
+            gameObject.transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+    }
+}
